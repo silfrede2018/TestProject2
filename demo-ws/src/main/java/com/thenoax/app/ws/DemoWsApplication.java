a68bc19de55dd7b20c9166a2071dee2d6adc13d8
silfrede2018 <33774039+silfrede2018@users.noreply.github.com>
2019-01-09 17:47:49 +0800

    Change

--- demo-ws/src/main/java/com/thenoax/app/ws/DemoWsApplication.java
+++ demo-ws/src/main/java/com/thenoax/app/ws/DemoWsApplication.java
@@ -8,7 +8,10 @@ public class DemoWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWsApplication.class, args);
+		System.out.println("First");
 	}
+	
+
 
 }
 
